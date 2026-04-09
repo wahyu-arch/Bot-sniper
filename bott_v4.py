3ff2d353b9624597235a7a6da411c5cd3606e2ae
wahyu-arch <calon.eleh12@gmail.com>
2026-04-09 18:05:01 +0700

    Add files via upload

--- bott_v4.py
+++ bott_v4.py
@@ -497,7 +497,7 @@ def run_bot():
                 df_h1_live = get_data(coin, "60", limit=150)
                 if df_h1_live is None: continue
 
-                sh_h1, sl_h1 = find_swings(df_h1_live, left=8, right=8)
+                sh_h1, sl_h1 = find_swings(df_h1_live, left=25, right=25)
                 if not sh_h1 or not sl_h1: continue
 
                 curr_h1   = df_h1_live.iloc[-1]
